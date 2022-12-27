4778ba41b085894d3e93594f2e3189b6790cf565
mvm <mhxy13867806343@users.noreply.github.com>
2022-12-27 16:20:44 +0800

    新增聊天静态页面

--- utils/tools.py
+++ utils/tools.py
@@ -4,7 +4,7 @@ def osFilePathIsdir(_path:str="uploads"):
     year = time.strftime("%Y", time.localtime())
     moment = time.strftime("%m", time.localtime())
     # 年    月      ID      文件
-    ps = f"uploads/{_path}"
+    ps = f"file/{_path}"
     p = os.path.isdir(ps)
     if not p:
         os.makedirs(ps)
